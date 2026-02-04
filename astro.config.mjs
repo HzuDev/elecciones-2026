54f5185c9f1c11946d635e594321411cccc00698
HzuDev <wante.relax@gmail.com>
2026-02-04 13:54:07 -0400

    fix: corregir base path en la configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,19 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  // 1. Agregamos la URL de tu sitio y el subdirectorio (nombre del repo)
+  // Asegúrate de que estas dos líneas estén así:
   site: 'https://hzudev.github.io',
-  base: '/elecciones-2026pnpm', 
+  base: '/elecciones-2026', 
 
   vite: {
     plugins: [tailwindcss()],
     define: {
-      // Nota: Astro ya maneja variables .env de forma nativa si empiezan con PUBLIC_
-      // Pero si prefieres mantener este bloque, asegúrate de que existan en GitHub Secrets
       'import.meta.env.VITE_APPWRITE_ENDPOINT': JSON.stringify(process.env.VITE_APPWRITE_ENDPOINT),
       'import.meta.env.VITE_APPWRITE_PROJECT_ID': JSON.stringify(process.env.VITE_APPWRITE_PROJECT_ID),
       'import.meta.env.VITE_APPWRITE_DATABASE_ID': JSON.stringify(process.env.VITE_APPWRITE_DATABASE_ID),
       'import.meta.env.VITE_APPWRITE_BUCKET_ID': JSON.stringify(process.env.VITE_APPWRITE_BUCKET_ID),
     },
   },
-
   integrations: [react()],
 });
